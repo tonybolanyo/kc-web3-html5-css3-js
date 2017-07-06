77e8bcc12f702b4526d772bd0a5504e366bb5d71
Tony G. Bolaño <tonybolanyo@gmail.com>
2017-07-07 01:04:46 +0200

    Playing with navigation details (back top button)

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,7 @@
 
 var header = document.getElementById('header');
 var navbar = document.getElementById('navbar');
+var backTop = document.getElementById('back-top');
 
 // we stick navbar when reaches top of screen
 // as navbar is "inside" height of cover
@@ -9,14 +10,16 @@ var navbar = document.getElementById('navbar');
 var limit = header.getBoundingClientRect().height - navbar.getBoundingClientRect().height;
 
 window.addEventListener("scroll", function() {
-    var scrollY = window.scrollY;
+    var scrollY = window.pageYOffset;
     // when scrolling is beyond top of navbar
     // add a class to make it sticky
     // if not, remove that class
     if (scrollY > limit) {
         navbar.classList.add("fixed-navbar");
+        backTop.classList.add("visible");
     } else {
         navbar.classList.remove("fixed-navbar");
+        backTop.classList.remove("visible");
     }
 });
 
