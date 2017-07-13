e6f720abaf0b259470e474f45cde219ccfceffb5
Antonio G. Bolaño <angonzalez@odec.es>
2017-07-13 14:06:51 +0200

    Added simple trivia question loading through AJAX

--- js/trivia.js
+++ js/trivia.js
@@ -3,6 +3,9 @@ var question = document.getElementById("trivia-question");
 var correct = document.getElementById("trivia-ok");
 var fail = document.getElementById("trivia-fail");
 var closebtn = document.getElementsByClassName("close-btn")[0];
+var sessionToken = null;
+var currentQuestion = null;
+var endpoint = "https://opentdb.com/api.php?amount=1&category=18&difficulty=medium&type=boolean";
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", checkResponse);
@@ -13,11 +16,13 @@ closebtn.addEventListener("click", function() {
 });
 
 function checkResponse(event) {
-    if (event.target.value === 'true') {
+    console.log(event.target.value.toLowerCase(), currentQuestion.correct_answer.toLowerCase());
+    if (event.target.value.toLowerCase() === currentQuestion.correct_answer.toLowerCase()) {
         correct.classList.add("visible");
     } else {
         fail.classList.add("visible");
     }
+    loadQuestion();
     setTimeout(function() {
         correct.classList.remove("visible");
         fail.classList.remove("visible");
@@ -25,7 +30,21 @@ function checkResponse(event) {
 }
 
 function loadQuestion() {
-    question.innerText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, laudantium dicta eveniet sit veritatis unde! Voluptatem, rerum temporibus quod laudantium accusamus praesentium expedita ipsum beatae molestias aut laboriosam quae, amet?";
+    console.log("Loading question");
+    var	xhr = new XMLHttpRequest();
+    xhr.open("GET", endpoint, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var jsonResponse = JSON.parse(xhr.responseText);
+                currentQuestion = jsonResponse.results[0];
+                question.innerHTML = currentQuestion.question;
+            } else {
+                console.error("Error getting a new question", xhr.status);
+            }
+        }
+    }
+    xhr.send();
 }
 
 window.addEventListener("load", function(event) {
